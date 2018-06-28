143ddbf433725adf4a1187917f79d82c596eaa8e
ydi <ydi@ciklum.net>
2018-06-28 11:22:35 +0300

    small fixes

--- src/main/java/api/BaseApi.java
+++ src/main/java/api/BaseApi.java
@@ -3,7 +3,10 @@ package api;
 import org.aeonbits.owner.ConfigFactory;
 import utils.OpenWeatherConfig;
 
+
 public abstract class BaseApi {
+
+    private BaseApi() {}
 
     protected static OpenWeatherConfig openWeatherConfig = ConfigFactory.create(OpenWeatherConfig.class);
 }
